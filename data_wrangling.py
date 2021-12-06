c8a2325965958ce53f98dfbd59b9f07778b525cf
jjaensch <jakob.jr.96@gmail.com>
2021-12-06 07:27:31 +0100

    Unique print statement i spyder, excel ark der extracter df fra spyder

--- data_wrangling.py
+++ data_wrangling.py
@@ -24,9 +24,23 @@ df[['Month', 'Date']] = df['Date'].str.split(' ', 1, expand=True)
 
 
 # %%
+#
 df.dropna(inplace=True)
 #%%
+#
 df[['Cr_city', 'Cr_country']] = df['Crash_location'].str.split(', ', 1, expand=True)
 df[['Cr_region', 'Cr_country']] = df['Cr_country'].str.split(', ', 1, expand=True)
 df = df.drop(columns= ['Crash_location','Route'])
+#%%
+#Print statement for all collumn uniques and count of number of collumn variables
+for col in df:
+    print('For column',col,'number of uniques are:')
+    print(len(df[col].value_counts(dropna=False)))
+    print('-The uniques are:')
+    print(df[col].unique())
+    print('...')
+
+
 # %%
+#df.to_excel('Wrangled_data.xlsx')
+
