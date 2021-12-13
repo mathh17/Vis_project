82b5c5c975667515b9bc966351a3bb68801d7b2a
Mathias <mathh17@student.sdu.dk>
2021-12-13 13:55:28 +0100

    Maps done

--- planes_dashboard.py
+++ planes_dashboard.py
@@ -7,35 +7,29 @@ from dash import html
 import plotly.express as px
 #%%
 
-data = pd.read_csv(r'Visualisering\Vis_project\trimmed_crashes_city5k.csv')
+data = pd.read_csv(r'Visualisering\Vis_project\crashes_to_visualize.csv')
 data = data.drop(data.columns[0], axis=1)
 
-# %%
+#%%
+#data["total_passengers_dead"].astype(int)
+long = data["Crash_location_city_longitude"]
+
+lat = data["Crash_location_city_latitude"]
 #%%
 app = dash.Dash(__name__)
-"""
-types = data['Organisation'].unique()
-type_options = [{'label': i, 'value': i} for i in types]
-type_options.append({'label': 'All Volcano Types', 'value': 'all'})
-
-rocks = data['major_rock_1'].unique()
-rock_options = [{'label': i, 'value': i} for i in rocks]
-rock_options.append({'label': 'All Rock Types', 'value': 'all'})
 
 # mintime = data['last_eruption_year'].min()
 # maxtime = data['last_eruption_year'].max()
 mintime = 1900
 maxtime = 2030
-"""
-app.layout =    html.Div([
+
+app.layout =html.Div([    
+                html.Div([
                      html.Div([
                          dcc.Graph(id='crash-map')
-                     ],style={'width':'46%','display':'inline-block','vertical-align':'top','margin':'2%'}),
-                     html.Div([
-                         html.Div(id='miframe')
                      ],style={'width':'46%','display':'inline-block','vertical-align':'top','margin':'2%'})
                  ]),
-                html.Div([
+                 html.Div([
                      dcc.RangeSlider(
                        id='time-slider',
                        min=mintime,
@@ -44,38 +38,39 @@ app.layout =    html.Div([
                        value=[mintime,maxtime],
                        marks={i: str(i) for i in range(mintime, maxtime, 500)})
                  ])
+],style={'width':'100%'})    
 
 
 @app.callback(
     Output(component_id='crash-map', component_property='figure'),
     [
-        #Input(component_id='volcano_types', component_property='value'),
+        #Input(component_id='Organisation', component_property='value'),
         #Input(component_id='volcano_rocks', component_property='value'),
-        #Input(component_id='time-slider', component_property='value')
+        Input(component_id='time-slider', component_property='value')
     ]
 )
-def update_output(volcano_type, volcano_rock, time):
+def update_output(time):
     mydata = data
-    """
-    if volcano_type != 'all':
-        mydata = data[data['primary_volcano_type'] == volcano_type]
-    if volcano_rock != 'all':
-        mydata = mydata[mydata['major_rock_1'] == volcano_rock]
-    if time != [mintime,maxtime]:
-        mydata = mydata[mydata['last_eruption_year'] >= time[0]]
-        mydata = mydata[mydata['last_eruption_year'] <= time[1]]
-        """
     fig = px.scatter_mapbox(data_frame=mydata, 
-                        lat="Crash_location_city_latitude",
-                        lon="Crash_location_city_longitude",
-                        hover_name="Flight_num",
-                        size=[len(data)],
-                        size_max=10,
-                        zoom=0,
-                        height=1000)
-    fig.update_layout(mapbox_style="open-street-map")
+                        lat=long,
+                        lon=lat,
+                        hover_data=["Organisation",
+                                    "Crash_location_country",
+                                    "Crash_location_city",
+                                    "Date",
+                                    "Year",
+                                    "Ac_type"],
+                        color='Total dead',
+                        color_continuous_scale=px.colors.sequential.Bluered,
+                        size='Total dead',
+                        size_max=20,
+                        zoom=1,
+                        width = 1000,
+                        height=800)
+    print("plotly express hovertemplate:", fig.data[0].hovertemplate)
+    fig.update_traces(hovertemplate = "<br>Organisation: %{customdata[0]}<br>Country: %{customdata[1]}<br>City: %{customdata[2]}<br>Date: %{customdata[3]} %{customdata[4]}<br>Aircraft type: %{customdata[5]}<extra></extra>")
+    fig.update_layout(mapbox_style='carto-positron', autosize=False)
     fig.update_layout(margin={"r":0,"t":0,"l":20,"b":0})
     return fig
-
 if __name__ == '__main__':
     app.run_server(debug=True, port=8080)
